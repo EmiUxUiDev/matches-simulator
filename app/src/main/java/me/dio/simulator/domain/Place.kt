d38b76b7efaf9b882905c471f2a90cdf84afc15a
unknown <emilianoagu@hotmail.com>
2022-07-22 09:25:32 -0300

    Implementação da tela de detalles, finalização da app

---
+++ app/src/main/java/me/dio/simulator/domain/Place.kt
@@ -0,0 +1,13 @@
+package me.dio.simulator.domain
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Place(
+    @SerializedName("nome")
+    val name: String,
+    @SerializedName("imagem")
+    val image: String
+): Parcelable
